5542e95f3f9b9e905bcd40a046ec08a219f16ba5
Daniela Arenas Monroy <64091880+daniela20-m@users.noreply.github.com>
2020-04-28 00:09:31 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,38 @@
+package calculadora;
+
+/**
+ *
+ * @author danie_d8mi
+ */
+public class Principal {
+
+    public static int sumar (int a, int b){
+        return a+b;
+    }
+    
+    public static int resta (int a, int b){
+        return a-b;
+    }
+    
+    public static int multiplicacion (int a, int b){
+        return a*b;
+    }
+    
+    public static float division (float a, float b){
+        float div=0;
+        try{
+            div=a/b;
+        } catch (ArithmeticException e) {
+            System.out.print("No se puede dividir entre cero");
+        } 
+        return div; 
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Suma= "+sumar(10, 20));
+        System.out.println("Resta= "+resta(10, 20));
+        System.out.println("Multiplicacion= "+multiplicacion(10, 20));
+        System.out.println("Division= "+division(10, 5));
+    }
+    
+}
