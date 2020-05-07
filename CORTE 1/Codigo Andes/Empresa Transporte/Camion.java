5b118035d1047155b629fb496792285b96e6561a
Daniela Arenas Monroy <64091880+daniela20-m@users.noreply.github.com>
2020-05-07 18:57:55 -0500

    Rename CORTE 1/Camion.java to CORTE 1/Codigo Andes/Empresa Transporte/Camion.java

--- CORTE 1/Codigo Andes/Empresa Transporte/Camion.java
+++ CORTE 1/Codigo Andes/Empresa Transporte/Camion.java
@@ -1,7 +1,7 @@
 /**
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
- * Universidad de los Andes (Bogot· - Colombia)
- * Departamento de IngenierÌa de Sistemas y ComputaciÛn 
+ * Universidad de los Andes (Bogot√° - Colombia)
+ * Departamento de Ingenier√≠a de Sistemas y Computaci√≥n 
  * Licenciado bajo el esquema Academic Free License version 2.1 
  *
  * Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
@@ -12,7 +12,7 @@
 package uniandes.cupi2.empresaTransporte.mundo;
 
 /**
- * CamiÛn de la empresa de transporte.
+ * Cami√≥n de la empresa de transporte.
  */
 public class Camion {
 
@@ -21,22 +21,22 @@ public class Camion {
 	// -----------------------------------------------------------------
 
 	/**
-	 * Matricula del camiÛn.
+	 * Matricula del cami√≥n.
 	 */
 	private String matricula;
 
 	/**
-	 * Capacidad del camiÛn (en Kg).
+	 * Capacidad del cami√≥n (en Kg).
 	 */
 	private int capacidad;
 
 	/**
-	 * Consumo de gasolina del camiÛn (en galones de gasolina/kilÛmetro).
+	 * Consumo de gasolina del cami√≥n (en galones de gasolina/kil√≥metro).
 	 */
 	private double consumo;
 
 	/**
-	 * Carga actual del camiÛn (en Kg).
+	 * Carga actual del cami√≥n (en Kg).
 	 */
 	private int cargaActual;
 
@@ -45,15 +45,15 @@ public class Camion {
 	// -----------------------------------------------------------------
 
 	/**
-	 * Construye un nuevo camiÛn con los . <br>
-	 * <b>post: </b> La matrÌcula, la capacidad y el consumo fueron inicializados
-	 * con los valores dados por par·metro. <br>
-	 * La carga actual del camiÛn fue inicializada en 0.
+	 * Construye un nuevo cami√≥n con los . <br>
+	 * <b>post: </b> La matr√≠cula, la capacidad y el consumo fueron inicializados
+	 * con los valores dados por par√°metro. <br>
+	 * La carga actual del cami√≥n fue inicializada en 0.
 	 * 
-	 * @param pMatricula Matricula del camiÛn. pMatricula != null && pMatricula !=
+	 * @param pMatricula Matricula del cami√≥n. pMatricula != null && pMatricula !=
 	 *                   "".
-	 * @param pCapacidad Capacidad (en Kg.) del camiÛn. pCapacidad > 0.
-	 * @param pConsumo   Consumo de gasolina (en gasolina/kilÛmetro). pConsumo > 0.
+	 * @param pCapacidad Capacidad (en Kg.) del cami√≥n. pCapacidad > 0.
+	 * @param pConsumo   Consumo de gasolina (en gasolina/kil√≥metro). pConsumo > 0.
 	 */
 	public Camion(String pMatricula, int pCapacidad, double pConsumo) {
 		matricula = pMatricula;
@@ -63,11 +63,11 @@ public class Camion {
 	}
 
 	// -----------------------------------------------------------------
-	// MÈtodos
+	// M√©todos
 	// -----------------------------------------------------------------
 
 	/**
-	 * MÈtodo para instanciar clase
+	 * M√©todo para instanciar clase
 	 * 
 	 * @return void.
 	 */
@@ -76,47 +76,47 @@ public class Camion {
 	}
 
 	/**
-	 * Retorna la capacidad del camiÛn.
+	 * Retorna la capacidad del cami√≥n.
 	 * 
-	 * @return Capacidad del camiÛn.
+	 * @return Capacidad del cami√≥n.
 	 */
 	public int darCapacidad() {
 		return capacidad;
 	}
 
 	/**
-	 * Retorna el consumo de gasolina del camiÛn.
+	 * Retorna el consumo de gasolina del cami√≥n.
 	 * 
-	 * @return Consumo de gasolina del camiÛn.
+	 * @return Consumo de gasolina del cami√≥n.
 	 */
 	public double darConsumo() {
 		return consumo;
 	}
 
 	/**
-	 * Retorna la matricula del camiÛn.
+	 * Retorna la matricula del cami√≥n.
 	 * 
-	 * @return Matricula del camiÛn.
+	 * @return Matricula del cami√≥n.
 	 */
 	public String darMatricula() {
 		return matricula;
 	}
 
 	/**
-	 * Retorna la carga actual del camiÛn.
+	 * Retorna la carga actual del cami√≥n.
 	 * 
-	 * @return Carga actual del camiÛn.
+	 * @return Carga actual del cami√≥n.
 	 */
 	public int darCargaActual() {
 		return cargaActual;
 	}
 
 	/**
-	 * Carga el camiÛn con la carga dada por par·metro. <br>
-	 * <b>post: </b> El camiÛn fue cargado con el valor dado por par·metro.
+	 * Carga el cami√≥n con la carga dada por par√°metro. <br>
+	 * <b>post: </b> El cami√≥n fue cargado con el valor dado por par√°metro.
 	 * 
 	 * @param pPesoCarga Peso a cargar (en Kg). pPesoCarga > 0.
-	 * @return Retorna true si logra cargar el camiÛn, false de lo contrario.
+	 * @return Retorna true si logra cargar el cami√≥n, false de lo contrario.
 	 */
 	public boolean cargar(int pPesoCarga) {
 		boolean cargo = false;
@@ -128,8 +128,8 @@ public class Camion {
 	}
 
 	/**
-	 * Descarga el camiÛn. <br>
-	 * <b>post: </b> La carga del camiÛn es 0.
+	 * Descarga el cami√≥n. <br>
+	 * <b>post: </b> La carga del cami√≥n es 0.
 	 */
 	public void descargar() {
 		cargaActual = 0;
